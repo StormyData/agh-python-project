a2f9f967fb36ba3aae6c7a6595207e80bd865e47
Tobiasz Szulc <szulc.tobiasz@gmail.com>
2022-04-06 21:30:24 +0200

    implemented image loading part of assetloader

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.Parser import parse_file
-
+from src.AssetLoader import AssetLoader
 
 def main():
+    AssetLoader.get_singleton().load_paths("assets/descriptor.xml")
+    print(AssetLoader.get_singleton().get_image("grass0"))
     level = parse_file("levels/level01.xml")
     return level
 
--- src/AssetLoader.py
+++ src/AssetLoader.py
@@ -1,13 +1,43 @@
-from pygame import Surface
+import xml.etree.ElementTree as ETree
+from pygame import Surface,image
 
+from src.Parser import ParseError
 
 class AssetLoader:
+    _instance = None
+
     @staticmethod
     def get_singleton():
-        pass
+        if AssetLoader._instance is None:
+            AssetLoader._instance = AssetLoader()
+        return AssetLoader._instance
+
+    def __init__(self):
+        self._images = dict()
+        self._image_names_to_paths = dict()
+
+    def load_paths(self, fp: str):
+        tree = ETree.parse(fp)
+        root = tree.getroot()
+        if root.tag != "assets":
+            raise ParseError("root must be named assets")
+        for child in root:
+            match child.tag:
+                case "img":
+                    if 'path' not in child.attrib:
+                        raise ParseError("img is missing attribute: path")
+                    if 'name' not in child.attrib:
+                        raise ParseError("img is missing attribute: name")
+                    self._image_names_to_paths[child.attrib['name']] = child.attrib['path']
+                case _:
+                    pass  # ignore
 
     def get_image(self, name: str) -> Surface:
-        pass
+        if name not in self._images:
+            if name not in self._image_names_to_paths:
+                raise ValueError(f"unknown image name: {name}")
+            self._images[name] = image.load(self._image_names_to_paths[name], "")
+        return self._images[name]
 
     def get_sound_buffer(self, name: str) -> bytes:
         pass
