58971f3b700d78860ff82685fc42d8e9d953c679
Tobiasz Szulc <szulc.tobiasz@gmail.com>
2022-05-04 22:22:50 +0200

    added test case for previously encountered problem

--- tests/Collider.py
+++ tests/Collider.py
@@ -15,6 +15,11 @@ class ColliderTestCase(unittest.TestCase):
         self.assertFalse(c.collides(a), f"Colliders {c} and {a} did collide")
         self.assertFalse(b.collides(c), f"Colliders {b} and {c} did collide")
         self.assertFalse(c.collides(b), f"Colliders {c} and {b} did collide")
+        c.move_by(Vector(-1.5, -1.5))
+        self.assertTrue(a.collides(c), f"Colliders {a} and {c} did not collide")
+        self.assertTrue(c.collides(a), f"Colliders {c} and {a} did not collide")
+        self.assertFalse(b.collides(c), f"Colliders {b} and {c} did collide")
+        self.assertFalse(c.collides(b), f"Colliders {c} and {b} did collide")
 
     def test_collide_offset(self):
         a = Collider(Vector(0, 0), Vector(5, 5))
@@ -24,13 +29,18 @@ class ColliderTestCase(unittest.TestCase):
         self.assertEqual(expected1, a.collide_offset(b), f"incorrect offset for {a} and {b}")
         self.assertEqual(expected2, b.collide_offset(a), f"incorrect offset for {b} and {a}")
 
-        c = Collider(Vector(5, 5), Vector(1, 1))
+        c = Collider(Vector(6, 6), Vector(1, 1))
         expected3 = Vector(0, 0)
         self.assertEqual(expected3, a.collide_offset(c), f"incorrect offset for {a} and {c}")
         self.assertEqual(expected3, c.collide_offset(a), f"incorrect offset for {c} and {a}")
         self.assertEqual(expected3, b.collide_offset(c), f"incorrect offset for {b} and {c}")
         self.assertEqual(expected3, c.collide_offset(b), f"incorrect offset for {c} and {b}")
-
+        c.move_by(Vector(-1.5, -1.5))
+        expected4 = Vector(0, 0.5)
+        self.assertEqual(-expected4, a.collide_offset(c), f"incorrect offset for {a} and {c}")
+        self.assertEqual(expected4, c.collide_offset(a), f"incorrect offset for {c} and {a}")
+        self.assertEqual(expected3, b.collide_offset(c), f"incorrect offset for {b} and {c}")
+        self.assertEqual(expected3, c.collide_offset(b), f"incorrect offset for {c} and {b}")
 
 if __name__ == '__main__':
     unittest.main()
